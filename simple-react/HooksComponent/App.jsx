5468011876c373270f64f8aa1a003500c82b3906
Abdullah Al Noman <abdullahalnomancse@gmail.com>
2024-02-04 14:07:52 +0600

    added new thing

---
+++ simple-react/HooksComponent/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <div
+        style={{
+          fontFamily: "Honk",
+          fontSize: "2rem",
+          textTransform: "capitalize",
+          letterSpacing:'2px'
+        }}
+      >
+        Hello App
+      </div>
+      <p style={{ fontSize: "12px", color: "#280274" }}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores quas
+        nemo dignissimos earum nostrum mollitia molestias. Saepe dolorum dolorem
+        optio cum repellat nam laborum eos eveniet, sequi temporibus vel hic.
+      </p>
+    </>
+  );
+};
+
+export default App;
--- simple-react/src/main.jsx
+++ simple-react/src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
+import "./index.css";
 import EventApp from "./EventApp.jsx";
 import DateStaeCount from "./DateStaeCount.jsx";
 import FarAwaye from "../components/FarAwaye.jsx";
 import Student from "./Student.jsx";
-import App from "../componentsForPayApps/App.jsx";
+import App from "../EatNSplitComponent/App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
